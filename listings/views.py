bf20370e20d8a664f9418d5a8fa8cf6e493b08c1
Lili <felixtsui@outlook.com>
2017-03-29 11:45:27 -0400

    googlemap_py

--- listings/views.py
+++ listings/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 # Create your views here.
+import googlemaps
+
+gmaps = googlemaps.Client(key='#')
 
 
 def index(request):
     keyword, roomtype = request.GET.get('keyword'), request.GET.get('roomtype')
     if keyword and roomtype:
-        print(keyword, roomtype)
-    return render(request, 'listings/index.html')
+        geocode_result = gmaps.geocode(keyword + ' ' + 'New York')[0]
+        address = geocode_result['formatted_address']
+        geometry = geocode_result['geometry']['location']
+        return render(request, 'listings/index.html', {'address': address, 'geometry': geometry})
+    else:
+        return render(request, 'listings/index.html')
