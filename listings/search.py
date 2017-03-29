c48996300316f4e54323b78a25e650ec34056cea
Lili <felixtsui@outlook.com>
2017-03-29 14:09:35 -0400

    search_for_nearby_listings

--- listings/search.py
+++ listings/search.py
@@ -1,13 +1,31 @@
 from elasticsearch import Elasticsearch, RequestsHttpConnection
-from requests_aws4auth import AWS4Auth
+#from requests_aws4auth import AWS4Auth
+import math
 
-host = ''
-awsauth = AWS4Auth('', '', 'us-east-1', 'es')
+host = 'search-listings-copyetrhi7huiyy2bd57lzrrdm.us-east-1.es.amazonaws.com'
+#awsauth = AWS4Auth('', '', 'us-east-1', 'es')
 
 es = Elasticsearch(
     hosts=[{'host': host, 'port': 443}],
-    http_auth=awsauth,
+    #http_auth=awsauth,
     use_ssl=True,
     verify_certs=True,
     connection_class=RequestsHttpConnection
 )
+
+
+def distance(lat1, lon1, lat2, lon2):
+    dlat = deg2rad(lat2-lat1)
+    dlon = deg2rad(lon2-lon1)
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(deg2rad(lat1)) * math.cos(deg2rad(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    return 6371 * c
+
+
+def deg2rad(deg):
+    return deg * (math.pi/180)
+
+
+def search(lat_a, lon_a, limit):
+    result = es.search(index='listings', size=100)['hits']['hits']
+    return [x for x in result if distance(lat_a, lon_a, float(x['_source']['latitude']), float(x['_source']['longitude'])) <= limit]
--- listings/views.py
+++ listings/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from . import search
 # Create your views here.
 import googlemaps
 
-gmaps = googlemaps.Client(key='#')
+gmaps = googlemaps.Client(key='AIzaSyAzqsrd9qkXn7qoQSJJwCkhIMM77OffJSI')
 
 
 def index(request):
@@ -11,6 +12,8 @@ def index(request):
         geocode_result = gmaps.geocode(keyword + ' ' + 'New York')[0]
         address = geocode_result['formatted_address']
         geometry = geocode_result['geometry']['location']
-        return render(request, 'listings/index.html', {'address': address, 'geometry': geometry})
+        count = len(search.search(geometry['lat'], geometry['lng'], 5))
+        return render(request, 'listings/index.html', {'address': address, 'geometry': geometry, 'count': count})
     else:
         return render(request, 'listings/index.html')
+
